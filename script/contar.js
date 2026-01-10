7171f4b714ad63ec16dfa089badecbdac3ec765b
Michael Douglas Alves Machado <michaeldouglasalvesmachado@gmail.com>
2026-01-10 01:36:46 -0300

    treino de js - contar

---
+++ script/contar.js
@@ -0,0 +1,33 @@
+const btnContar = document.querySelector('#btnContar')
+const resultado = document.querySelector('.resultadoContar')
+
+btnContar.addEventListener('click', contar)
+function contar() {
+	var inicio = document.querySelector('#iinicio')
+	var fim = document.querySelector('#ifim')
+	var passo = document.querySelector('#ipasso')
+    resultado.innerHTML = ''
+	if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 || passo.value == 0) {
+        alert('Erro: Número impossível')
+    } else {
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        passo = Number(passo.value)
+        var contagem = inicio
+        if (fim < inicio) {
+            while (contagem >= fim) {
+                var novoSpan = document.createElement('span')
+                novoSpan.innerHTML = (fim <= contagem - passo) ? `${contagem} Passo ` : `${contagem} Fim`
+                resultado.appendChild(novoSpan)
+                contagem = contagem - passo
+            }
+        } else {
+            while (contagem <= fim) {
+                var novoSpan = document.createElement('span')
+                novoSpan.innerHTML = (contagem <= fim) ? `${contagem} Passo ` : `${contagem} Fim`
+                resultado.appendChild(novoSpan)
+                contagem = contagem + passo
+            }
+        }
+    }
+}
