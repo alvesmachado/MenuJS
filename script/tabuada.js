ad1550a2c1806d0a9c7276a8029a07effc4c247d
Michael Douglas Alves Machado <michaeldouglasalvesmachado@gmail.com>
2026-01-13 00:35:21 -0300

    Esqueci de enviar (Ainda preciso entender mais sobre git)

--- script/tabuada.js
+++ script/tabuada.js
@@ -2,12 +2,17 @@ const tabuada = document.querySelector('#doaArt .tabuada')
 const btnMultiplicar = document.querySelector('#btnMulti')
 btnMultiplicar.addEventListener('click', multiplicar)
 function multiplicar() {
-    const multiplicando = Number(document.querySelector('#inputMultiplicando').value)
-    for (let multiplicador = 1;multiplicador <= 10;multiplicador++) {
-	const multiplicacao = multiplicando * multiplicador
-	const novaTabuada = document.createElement('span')
-	novaTabuada.className = 'tabuadaStyle'
-	novaTabuada.innerHTML = `${multiplicando} x ${multiplicador} = ${multiplicacao}`
-	tabuada.appendChild(novaTabuada)
-    }    
+    let multiplicando = document.querySelector('#inputMultiplicando')
+	tabuada.innerHTML = '<span class="tabuadaStyle">Tabuada</span>'
+	if (multiplicando.value.trim().length != 0){
+		for (let multiplicador = 1;multiplicador <= 10;multiplicador++) {
+			let multiplicacao = Number(multiplicando.value) * multiplicador
+			let novaTabuada = document.createElement('span')
+			novaTabuada.className = 'tabuadaStyle'
+			novaTabuada.innerHTML = `${multiplicando.value} x ${multiplicador} = ${multiplicacao}`
+			tabuada.appendChild(novaTabuada)
+    	} 
+	} else {
+		alert('Erro: Número vazio')
+	}      
 }
